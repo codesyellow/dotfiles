8b1a21ca70d8e95950e819a3353c71cad5111e38
cse <codesyellow@proton.me>
2023-01-19 13:09:19 -0300

    fix opacity for qutebrowser

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -43,6 +43,28 @@ def set_layout(qtile):
         qtile.cmd_to_layout_index(1)
         my_bar.show(False)
 
+#Colors for the bar
+def init_colors():
+    return [["#2e3440", "#2e3440"], # color 0  background color
+            ["#2e3440", "#2e3440"], # color 1  dark grayish blue
+            ["#3b4252", "#3b4252"], # color 2  very dark grayish blue
+            ["#434c5e", "#434c5e"], # color 3  very dark grayish blue
+            ["#4c566a", "#4c566a"], # color 4  very dark grayish blue
+            ["#d8dee9", "#d8dee9"], # color 5  grayish blue
+            ["#e5e9f0", "#e5e9f0"], # color 6  light grayish blue
+            ["#eceff4", "#eceff4"], # color 7  light grayish blue
+            ["#8fbcbb", "#8fbcbb"], # color 8  grayish cyan
+            ["#88c0d0", "#88c0d0"], # color 9  desaturated cyan
+            ["#81a1c1", "#81a1c1"], # color 10 desaturated blue
+            ["#5e81ac", "#5e81ac"], # color 11 dark moderate blue
+            ["#bf616a", "#bf616a"], # color 12 slightly desaturated red
+            ["#d08770", "#d08770"], # color 13 desaturated red
+            ["#ebcb8b", "#ebcb8b"], # color 14 soft orange
+            ["#a3be8c", "#a3be8c"], # color 15 desaturated green
+            ["#b48ead", "#b48ead"]] # color 16 grayish magenta
+
+colors = init_colors()
+
 def my_layouts():
     return [
             layout.MonadWide(
@@ -57,12 +79,13 @@ def my_layouts():
                 ),
             layout.Max(),
             layout.TreeTab(
-                bg_color=colors[0],
-                border_width=0,
-                place_right=True,
-                section_fg=colors[0],
                 active_bg=colors[5],
                 active_fg=colors[0],
+                border_width=0,
+                bg_color=colors[0],
+                inactive_bg=colors[0],
+                place_right=True,
+                section_fg=colors[0],
             ),
     ]
 
@@ -105,27 +128,7 @@ def my_widgets():
             widget.Spacer(length=6),
 ]
 
-#Colors for the bar
-def init_colors():
-    return [["#2e3440", "#2e3440"], # color 0  background color
-            ["#2e3440", "#2e3440"], # color 1  dark grayish blue
-            ["#3b4252", "#3b4252"], # color 2  very dark grayish blue
-            ["#434c5e", "#434c5e"], # color 3  very dark grayish blue
-            ["#4c566a", "#4c566a"], # color 4  very dark grayish blue
-            ["#d8dee9", "#d8dee9"], # color 5  grayish blue
-            ["#e5e9f0", "#e5e9f0"], # color 6  light grayish blue
-            ["#eceff4", "#eceff4"], # color 7  light grayish blue
-            ["#8fbcbb", "#8fbcbb"], # color 8  grayish cyan
-            ["#88c0d0", "#88c0d0"], # color 9  desaturated cyan
-            ["#81a1c1", "#81a1c1"], # color 10 desaturated blue
-            ["#5e81ac", "#5e81ac"], # color 11 dark moderate blue
-            ["#bf616a", "#bf616a"], # color 12 slightly desaturated red
-            ["#d08770", "#d08770"], # color 13 desaturated red
-            ["#ebcb8b", "#ebcb8b"], # color 14 soft orange
-            ["#a3be8c", "#a3be8c"], # color 15 desaturated green
-            ["#b48ead", "#b48ead"]] # color 16 grayish magenta
 
-colors = init_colors()
 
 # hooks
 @hook.subscribe.client_focus
@@ -137,7 +140,7 @@ def opacity(c):
             set_opacity(0.5) 
         else:
             set_opacity(1) 
-        if wm_class == 'firefox' or wm_class == 'qutebrowser':
+        if wm_class == 'firefox' or wm_class == 'org.qutebrowser.qutebrowser':
             set_opacity(1) 
 
 @hook.subscribe.startup_once
@@ -155,6 +158,8 @@ def center_float(c):
     ## if layout if max show number of windows on widgets
     ## focus main window
     ## hide cursor automacally
+
+## not transparency when scratchpad on top of a terminal window
 
 # rules
 wl_input_rules = {
@@ -204,6 +209,8 @@ keys = [
     KeyChord([mod], "a", [
         Key([], "h", lazy.core.hide_cursor()),
         Key([], "s", lazy.core.unhide_cursor()),
+        Key([], "e", lazy.spawn('easyeffects -l LoudnessEqualizer')),
+        Key([], "b", lazy.spawn('easyeffects -l "Heavy Bass"')),
     ]),
 ]
 
