420f0f5d026bd2e7e3cfaa9f50fcc0478e3bb862
cse <codesyellow@proton.me>
2023-02-03 12:33:56 -0300

    command to fix firefox

--- .xinitrc.d/autostart.sh
+++ .xinitrc.d/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 easyeffects --gapplication-service &
-xrandr --output VGA-1 --gamma 1.0:0.88:0.90 --brightness 0.95 &
+xrandr --output VGA-1 --gamma 1.0:0.88:0.50 --brightness 0.95 &
 hsetroot -cover ~/.wallpapers/house.jpg &
 xset led on &
 keyd-application-mapper -d
@@ -14,3 +14,5 @@ cpupower frequency-set -g performance &
 run_xidlehook &
 easyeffects -l 'LoudnessEqualizer' &
 alsactl --file ~/.config/asound.state restore &
+# this is for fixing firefox slow startup
+dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY &
