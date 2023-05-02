702d8818e43f776f604d6ad2f832bb7ce42b579a
codesyellow <codesyellow@protonme.com>
2023-05-02 08:58:37 -0300

    picom changes

--- .xinitrc.d/autostart.sh
+++ .xinitrc.d/autostart.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
-easyeffects --gapplication-service &
 xrandr --output VGA-1 --gamma 1.0:0.88:0.50 --brightness 0.95 &
-hsetroot -cover ~/.wallpapers/house.jpg &
+hsetroot -cover ~/.wallpapers/forest_stairs.jpg &
 xset led on &
 keyd-application-mapper -d
 dunst &
 unclutter &
 run_xidlehook &
 safeeyes &
-picom --experimental-backend &
+picom --animations --focus-exclude "! name~=''" &
 nm-applet &
 cpupower frequency-set -g performance &
 run_xidlehook &
 easyeffects -l 'LoudnessEqualizer' &
-alsactl --file ~/.config/asound.state restore &
-# this is for fixing firefox slow startup
 dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY &
+alsactl --file ~/.config/asound.state restore &
+easyeffects --gapplication-service &
 emulate_xbox.sh &
-alacritty &
