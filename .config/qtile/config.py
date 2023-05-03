f657bd024ca967dc81868e1a973e9df6ee62ab9a
codesyellow <codesyellow@protonme.com>
2023-05-03 08:32:18 -0300

    changes

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -163,7 +163,7 @@ def my_widgets():
 def opacity(c):
     for x in c.qtile.current_group.windows:
         set_opacity = x.set_opacity
-        wm_class = x.get_wm_class()[0]
+        wm_class = x.get_wm_class()[1]
         if (wm_class == 'firefox' 
             or wm_class == 'org.qutebrowser.qutebrowser'
             or wm_class == 'qutebrowser'):
@@ -179,9 +179,11 @@ def opacity(c):
 @hook.subscribe.client_focus
 def is_floating(c):
     for x in c.qtile.current_group.windows:
-        if x.has_focus and x.floating and x.name != 'scratchpad':
-            if x.get_wm_class()[0] == 'firefox':
-                x.set_size_floating(500,680)
+        if x.has_focus and x.floating and x.name != 'scratchpad' and not x.fullscreen:
+            logger.warning(dir(x))
+            logger.warning(x.fullscreen)
+            if x.get_wm_class()[0] == 'Navigator':
+                x.set_size_floating(400,680)
             x.center()
         
 if qtile.core.name == "wayland":
