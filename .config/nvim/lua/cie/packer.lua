6890ee475fa04a3ad4032f0c949efcc92e6e8fff
cie <codesyellow@proton.me>
2023-07-16 19:23:43 -0300

    changes

--- .config/nvim/lua/cie/packer.lua
+++ .config/nvim/lua/cie/packer.lua
@@ -37,13 +37,7 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
 
-  use({
-      "neanias/everforest-nvim",
-      -- Optional; default configuration will be used if setup isn't called.
-      config = function()
-          require("everforest").setup()
-      end,
-  })
+  use { "catppuccin/nvim", as = "catppuccin" }
 
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
