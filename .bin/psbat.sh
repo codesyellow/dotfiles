1d45525b9b9920f8cea1f47d237a59927979db79
cie <codesyellow@proton.me>
2023-10-07 18:55:35 -0300

    changes

--- .bin/psbat.sh
+++ .bin/psbat.sh
@@ -2,5 +2,5 @@
 
 text=$(dsbattery)
 result=$(echo "$text" | grep -o '[0-9]*%')
-echo "$result"
+printf '%s' "$result"
 
--- .bin/recharge_ps4.sh
+++ .bin/recharge_ps4.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 while true; do
     bat_stat=$(ps4_status.sh perc | sed 's/%//')
+    echo $bat_stat
     if [[ $bat_stat -le '30' ]]; then
         notify-send "Battery is at $bat_stat!"                            
-        exit 1
     fi
-
-    sleep 300
     echo "Dont need charge! $bat_stat"
+    sleep 300
 done
 
