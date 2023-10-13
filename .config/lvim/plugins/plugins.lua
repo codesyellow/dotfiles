39247c5eed2396958be2014ca7030b47b7016566
cie <codesyellow@proton.me>
2023-10-13 16:43:46 -0300

    first commit

---
+++ .config/lvim/plugins/plugins.lua
@@ -0,0 +1,16 @@
+lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  {
+    "stevearc/dressing.nvim",
+    config = function()
+      require("dressing").setup({
+        input = { enabled = false },
+      })
+    end,
+  },
+  {
+    "nvim-neorg/neorg",
+    ft = "norg", -- lazy-load on filetype
+    config = true, -- run require("neorg").setup()
+  },
+}
