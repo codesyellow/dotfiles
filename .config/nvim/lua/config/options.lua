c13ae8f4703bd3c6eed74b9934c68957b358e4c0
cie <codesyellow@protonmail.com>
2024-07-13 11:31:47 -0300

    wrap set to true

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.nvim_tree_respect_buf_cwd = 1
+vim.o.wrap = true
