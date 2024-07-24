f16d8ffccda6a5ed7437d527b4634d6a2fcec2ee
cie <codesyellow@protonmail.com>
2024-07-24 10:29:43 -0300

    changes

--- .bin/emulate_xbox_ps4.sh
+++ .bin/emulate_xbox_ps4.sh
@@ -12,7 +12,7 @@ if [[ -n $is_active ]]; then
 else
   echo 'no'
   notify-send.sh -i ~/.local/share/icons/icons8-xbox-controller-50.png 'Xbox joy mode activated'
-  xboxdrv --evdev "/dev/input/event18" --evdev-keymap "BTN_SOUTH=a,BTN_EAST=b,BTN_TL=lb,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start,BTN_THUMBL=tl,BTN_TR=rb,BTN_NORTH=y,BTN_WEST=x" --evdev-absmap "ABS_RZ=rt,ABS_RY=y2,ABS_RX=x2,ABS_Z=lt,ABS_Y=y1,ABS_X=x1,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y" --axismap "-y2=y2" --mimic-xpad --silent
+  xboxdrv --evdev "/dev/input/event19" --evdev-keymap "BTN_SOUTH=a,BTN_EAST=b,BTN_TL=lb,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start,BTN_THUMBL=tl,BTN_TR=rb,BTN_NORTH=y,BTN_WEST=x" --evdev-absmap "ABS_RZ=rt,ABS_RY=y2,ABS_RX=x2,ABS_Z=lt,ABS_Y=y1,ABS_X=x1,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y" --axismap "-y2=y2" --mimic-xpad --silent
 fi
 
 #doas xboxdrv --evdev "/dev/input/event18" --evdev-keymap "BTN_SOUTH=a,BTN_EAST=b,BTN_TL=lb,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start,BTN_THUMBL=tl,BTN_TR=rb,BTN_NORTH=y,BTN_WEST=x" --evdev-absmap "ABS_RZ=rt,ABS_RY=y2,ABS_RX=x2,ABS_Z=lt,ABS_Y=y1,ABS_X=x1,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y" --axismap "-y2=y2,-y1=y1" --mimic-xpad --silent
--- .bin/sh_check_server.sh
+++ .bin/sh_check_server.sh
@@ -3,11 +3,19 @@ pkill -SIGRTMIN+8 waybar
 if [[ $1 = 'icon' ]]; then
   exit 1
 fi
+
 # Get the current hour (24-hour format)
 current_hour=$(date +"%H")
+server=$(check_server.sh)
 
-if [ "$current_hour" -ge 6 ] && [ "$current_hour" -le 22 ]; then
-  true
-elif [[ "$current_hour" -ge 22 ]]; then
+if [[ $server == "0" ]]; then
+  #  echo yes
   false
+else
+  if [ "$current_hour" -ge 5 ] && [ "$current_hour" -le 21 ]; then
+    true
+  else
+    false
+  fi
+#  echo no
 fi
