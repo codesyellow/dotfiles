26ad6949ac63fc35b5b4b55796c9246bd18bb795
cie <codesyellow@protonmail.com>
2024-08-09 11:43:57 -0300

    changes

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -3,11 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
@@ -22,15 +17,15 @@ static const char *fonts[]          = {
   "Font Awesome 6 Free Solid:size=11",
 };
 static const char dmenufont[]       = "JetBrainMono Nerd Font:size=10";
-static const char bg1[]       = "#322C2B";
-static const char col_gray2[]       = "#000000";
-static const char col_gray3[]       = "#E4C59E";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#543A48";
+static const char bg0[]       = "#2e3440";
+static const char bg1[]       = "#4c566a";
+static const char fg0[]       = "#d8dee9";
+static const char fg1[]       = "#eceff4";
+static const char border[]        = "#bf616a";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
-  [SchemeNorm] = { col_gray3, bg1, col_gray2 },
-  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeNorm] = { fg0, bg0, bg1 },
+  [SchemeSel]  = { fg1, border,  border  },
 };
 
 /* tagging */
@@ -82,9 +77,6 @@ static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-#include "vanitygaps.c"
 
 static const Layout layouts[] = {
   /* symbol     arrange function */
