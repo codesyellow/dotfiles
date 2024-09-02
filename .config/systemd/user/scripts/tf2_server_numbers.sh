3b3c9668bea62aca06889a1329f0b016a41e78fa
cie <codesyellow@protonmail.com>
2024-09-02 10:45:31 -0300

    changes

--- .config/systemd/user/scripts/tf2_server_numbers.sh
+++ .config/systemd/user/scripts/tf2_server_numbers.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 current_hour=$(date +%H)
-if [[ $current_hour -lt 6 || $current_hour -ge 22 ]]; then
-  systemctl --user stop tf2_server_count.timer
+if [[ $current_hour -lt 5 || $current_hour -ge 22 ]]; then
+  disable="/tmp/disable_server_info"
+  if [[ ! -f "$disable_server_info" ]]; then
+    touch "$disable"
+  fi
+  echo "It will not fetch data since it's past 22"
   exit 0
 fi
 
