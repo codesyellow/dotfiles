279add40a9667edafee337f054fc1f42f198bc31
cie <codesyellow@protonmail.com>
2024-09-13 10:50:42 -0300

    changed bstack layout to place the master on the bottom

--- .local/share/dwm/dwm.c
+++ .local/share/dwm/dwm.c
@@ -2868,47 +2868,50 @@ main(int argc, char *argv[])
 	checkotherwm();
 	setup();
 #ifdef __OpenBSD__
-	if (pledge("stdio rpath proc exec", NULL) == -1)
-		die("pledge");
+        if (pledge("stdio rpath proc exec", NULL) == -1)
+          die("pledge");
 #endif /* __OpenBSD__ */
-	scan();
-	runautostart();
-	run();
-	cleanup();
-	XCloseDisplay(dpy);
-	return EXIT_SUCCESS;
+        scan();
+        runautostart();
+        run();
+        cleanup();
+        XCloseDisplay(dpy);
+        return EXIT_SUCCESS;
 }
 
 static void
 bstack(Monitor *m) {
-	int w, h, mh, mx, tx, ty, tw;
-	unsigned int i, n;
-	Client *c;
+  int w, h, mh, mx, tx, ty, tw;
+  unsigned int i, n;
+  Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-	if (n > m->nmaster) {
-		mh = m->nmaster ? m->mfact * m->wh : 0;
-		tw = m->ww / (n - m->nmaster);
-		ty = m->wy + mh;
-	} else {
-		mh = m->wh;
-		tw = m->ww;
-		ty = m->wy;
-	}
-	for (i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		if (i < m->nmaster) {
-			w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), 0);
-			mx += WIDTH(c);
-		} else {
-			h = m->wh - mh;
-			resize(c, tx, ty, tw - (2 * c->bw), h - (2 * c->bw), 0);
-			if (tw != m->ww)
-				tx += WIDTH(c);
-		}
-	}
+  for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+  if (n == 0)
+    return;
+  if (n > m->nmaster) {
+    /* Change mh calculation to position master at the bottom */
+    mh = m->nmaster ? m->mfact * m->wh : 0;
+    tw = m->ww / (n - m->nmaster);
+    ty = m->wy; /* Start tiling from the top */
+  } else {
+    mh = m->wh;
+    tw = m->ww;
+    ty = m->wy;
+  }
+  for (i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+    if (i < m->nmaster) {
+      /* Position the master window at the bottom */
+      w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
+      resize(c, m->wx + mx, m->wy + m->wh - mh, w - (2 * c->bw), mh - (2 * c->bw), 0);
+      mx += WIDTH(c);
+    } else {
+      /* Tile the remaining windows above the master window */
+      h = m->wh - mh;
+      resize(c, tx, ty, tw - (2 * c->bw), h - (2 * c->bw), 0);
+      if (tw != m->ww)
+        tx += WIDTH(c);
+    }
+  }
 }
 
 static void
