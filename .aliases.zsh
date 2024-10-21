82a558722e6dcb2bfe10c84e401130d8febf24fc
digo <aedigo12@gmail.com>
2024-10-21 12:33:31 -0300

    changed and removed some aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,24 +1,23 @@
-sud=doas
+sud=sudo
 
-alias update-grub='$sud grub-mkconfig -o /boot/grub/grub.cfg'
-alias gtamod='~/.games/gta-sa/modloader/'
-function getInfo() {
+function :wi() {
   xprop | awk '
   /^WM_CLASS/{sub(/.* =/, "instance:"); sub(/,/, "\nclass:"); print}
   /^WM_NAME/{sub(/.* =/, "title:"); print}'
 }
 
 
-alias change_title='xdotool selectwindow set_window --name "esdf"'
+alias change_title='xdotool selectwindow set_window --name "scratchpad"'
 alias change_class='xdotool selectwindow set_window --class "esdf"'
 
 alias lp='{(comm -23 <(yay -Qqe | sort) <({(pactree -u -d 1 base)&&(yay -Qqg base-devel);} | sort))&&(comm -23 <(yay -Qqtt | sort) <(yay -Qqt | sort));} | sort | uniq  | less'
 
 # systemctl
-alias syst="systemctl --user start"
-alias systt="systemctl --user status"
-alias sysp="systemctl --user stop"
-alias sysr="systemctl --user restart"
+alias :st="systemctl --user start"
+alias :ss="systemctl --user status"
+alias :sp="systemctl --user stop"
+alias :sr="systemctl --user restart"
+alias :sd="systemctl --user daemon-reload"
 
 # dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
@@ -34,10 +33,10 @@ alias getWInfo='qtile cmd-obj -o cmd -f windows | less'
 alias search='pacman -Ss'
 alias lsm="exa -al --color=always --group-directories-first --icons"
 alias ls="exa --icons"
-alias up='yay && kill -31 $(cat ~/.cache/pidofbar)'
-alias i='doas pacman -S'
-alias r='doas pacman -Rs'
-alias orphans='$sud pacman -Qtdq | $sud pacman -Rns -'
+alias :pu='yay'
+alias :pi='sudo pacman -S'
+alias :pr='sudo pacman -Rs'
+alias :po='$sud pacman -Qtdq | $sud pacman -Rns -'
 
 # make
 alias smi='$sud rm config.h; make; $sud make install'
@@ -46,29 +45,30 @@ alias smi='$sud rm config.h; make; $sud make install'
 alias pbcopy='xclip -selection clipboard'
 
 # zsh
-alias sc='$EDITOR ~/.zshrc'
-alias sz='source ~/.zshrc'
+alias :sc='vim ~/.zshrc'
+alias :sz='source ~/.zshrc'
 
 # configs
-alias pc='$EDITOR ~/.config/picom/picom.conf'
+alias :cp='$EDITOR ~/.config/picom/picom.conf'
+alias :cz='$EDITOR ~/.zshrc'
+alias :cq='$EDITOR ~/.config/qtile/config.py'
+alias :cv='$EDITOR ~/.vimrc'
+alias :cvd='$EDITOR ~/.vim/defaults.vim'
 
 # cpupower
 alias boost="sudo cpupower frequency-set -g performance"
 alias powersave="sudo cpupower frequency-set -g powersave"
 
 # terminal apps
-alias sv='sudo -e'
-alias v='nvim'
-alias vimwiki='nvim ~/.vimwki/index.wiki'
-
+alias :v='$EDITOR'
 # Anki
 alias ae='canki.sh English'
 alias ap='canki.sh Portuguese'
 alias am='canki.sh Math'
 
 # translate shell
-alias t="trans -sp -brief en:pt-br"
-alias tb="trans -p -brief pt-br:en"
+alias :t="trans -sp -brief en:pt-br"
+alias :tb="trans -p -brief pt-br:en"
 
 # translate shell
 translate() {
