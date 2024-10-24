95aecbe48eb788d5eaea6015fe2d5d632dc2428b
digo <aedigo12@gmail.com>
2024-10-24 17:31:01 -0300

    change the position of the easy icon

--- .config/qtile/configs/widgets/easy.sh
+++ .config/qtile/configs/widgets/easy.sh
@@ -5,7 +5,7 @@ if [[ -n $is_easy_active ]]; then
   if [[ $easy = "LoudnessEqualizer" ]]; then
     printf '<span size="12000" foreground="#d8dee9" rise="3000"></span>'
   else
-    printf '<span size="15000" foreground="#EF5A6F" rise="5000"></span>'
+    printf '<span size="14500" foreground="#EF5A6F" rise="4100"></span>'
   fi
 else
   printf '<span size="12000" foreground="#EF5A6F" rise="3000">!</span>'
