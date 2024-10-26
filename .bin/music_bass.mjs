14b47718993f2749f08814658f8dfe6c9f204305
digo <aedigo12@gmail.com>
2024-10-25 21:57:39 -0300

    fixed bug when quiting cmus and reopening

--- .bin/music_bass.mjs
+++ .bin/music_bass.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env zx
 let active = false;
 let inactive = false;
+let off = false;
 
 const setEqualizer = async () => {
   await $`pamixer --set-volume 30`;
@@ -11,33 +12,37 @@ while (true) {
   try {
     const { stdout: cmusStatus } =
       await $`cmus-remote -Q 2>/dev/null | grep status | awk '{print $2}' || echo "0"`;
+    //    console.log(cmusStatus);
     if (cmusStatus.trim() === "playing" && !active) {
       active = true;
+      off = false;
       if (inactive) {
         inactive = false;
       }
       console.log("Playing");
       await $`easy_preset.sh 'HeavyBass'`;
     } else if (
-      cmusStatus.trim() === "paused" ||
+      (cmusStatus.trim() === "paused" && !inactive) ||
       (cmusStatus.trim() === "stopped" && !inactive)
     ) {
+      off = false;
       inactive = true;
-      console.log("Paused or stopped");
+      console.log("Paused or stopped", inactive);
       setEqualizer();
       if (active) {
         active = false;
       }
     } else if (cmusStatus.trim() === "0") {
-      if (!inactive) {
+      if (!off) {
         setEqualizer();
-        inactive = true;
+        console.log("not running!");
+        off = true;
       }
     }
   } catch (e) {
-    if (!inactive) {
+    if (!off) {
       setEqualizer();
-      inactive = true;
+      off = true;
     }
   }
   await sleep(2000);
