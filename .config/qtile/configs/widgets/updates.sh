fb5b5956bc16ef5e7b5168842ae06ed140c8d2c4
digo <aedigo12@gmail.com>
2024-10-26 19:07:08 -0300

    changed some runner settings and also fixed some bad widget position

--- .config/qtile/configs/widgets/updates.sh
+++ .config/qtile/configs/widgets/updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 updates_num=$(checkupdates | wc -l)
-if [[ $updates_num -ge 0 ]]; then
-  output=" $updates_num"
-  printf '<span rise="3000" foreground="#EF5A6F">%s</span> <span size="x-large" foreground="#fff">|</span>' "$output"
+if [[ $updates_num -ge 20 ]]; then
+  output="$updates_num"
+  printf '<span rise="3000" foreground="#EF5A6F"></span> <span rise="4300" foreground="#EF5A6F">%s</span> <span size="x-large" foreground="#fff">|</span>' "$output"
 fi
