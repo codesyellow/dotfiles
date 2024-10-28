6955e51cf3363e9b6c2155cc7cacd8c852f812e0
digo <aedigo12@gmail.com>
2024-10-28 05:52:28 -0300

    changes to xidlehook

--- .bin/xidlehook.sh
+++ .bin/xidlehook.sh
@@ -1,20 +1,25 @@
-#!/usr/bin/env bash
-
-# Only exported variables can be used within the timer's command.
+ #!/usr/bin/bash
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-
-# Run xidlehook
+#xset -dpms
 xidlehook \
-  `# Don't lock when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 60 seconds, undim if user becomes active` \
-  --timer 120 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+    `# Don't lock when there's a fullscreen application` \
+    --not-when-fullscreen \
+    `# Don't lock when there's audio playing` \
+    --not-when-audio \
+    `# Dim the screen after some minutes, undim if user becomes active` \
+    --timer 150\
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness .3' \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 300 \
+    `# screen off some minutes later, on & undim if user becomes active` \
+    --timer 150 \
+    'xset dpms force off' \
+    'xset dpms force on && xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+    `# Undim & lock after some minutes` \
+    --timer 600\
     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; slock' \
-    '' 
- 
+    '' #\
+    #`# Finally, suspend after it locks` \
+    #--timer 180 \
+    #'systemctl suspend' \
+    #''
+
