3bb50f609e968106323fa4322a86f43797cdf832
digo <aedigo12@gmail.com>
2024-10-30 15:32:23 -0300

    changed pymor and updates interval

--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -2,7 +2,7 @@
 
 updates_num=$(checkupdates | wc -l)
 
-if [[ $updates_num -ge 0 ]]; then
+if [[ $updates_num -ge 20 ]]; then
   state="warning"
   output=" $updates_num<span size='15000' foreground='#4c566a'> | </span>"
 fi
