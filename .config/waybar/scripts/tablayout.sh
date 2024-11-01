cacc807a4d9876fcd6e20059d73f3c6346d42b7c
digo <aedigo12@gmail.com>
2024-11-01 13:33:57 -0300

    removed floating rule for tabbed script

--- .config/waybar/scripts/tablayout.sh
+++ .config/waybar/scripts/tablayout.sh
@@ -5,10 +5,6 @@ clients=$(hyprctl activeworkspace | grep 'windows' | awk '{print $2}')
 client_name=$(hyprctl activeworkspace | grep 'lastwindowtitle' | awk '{print $2}')
 floating=$(hyprctl activewindow | grep 'floating' | awk '{print $2}')
 
-if [[ "$floating" -ge "1" ]]; then
-  ((clients--))
-fi
-
 if [[ "$clients" -ge "2" ]]; then
   state="warning"
   output="$clients_icon $clients [ $client_name ]<span size='15000' foreground='#4c566a'> | </span>"
