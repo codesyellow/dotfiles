12100114ee669f5f68c54ed9e91bc49c8b5a92d2
digo <aedigo12@gmail.com>
2024-11-07 17:05:56 -0300

    changed the position some modules and also fixed an issue with tablayout and steam

--- .config/waybar/scripts/tablayout.sh
+++ .config/waybar/scripts/tablayout.sh
@@ -2,8 +2,18 @@
 
 clients_icon=
 clients=$(hyprctl activeworkspace | grep 'windows' | awk '{print $2}')
-client_name=$(hyprctl activeworkspace | grep 'lastwindowtitle' | awk '{print $2}')
-floating=$(hyprctl activewindow | grep 'floating' | awk '{print $2}')
+client_name=$(hyprctl activeworkspace | awk '/lastwindowtitle/ {print $2; exit}')
+steamNotification=$(hyprctl clients | grep initialTitle | grep 'notificationtoasts_' | wc -l)
+steamMenu=$(hyprctl clients | grep -A 1 "initialClass: steam" | grep -c 'initialTitle')
+IsSteamMenu=$(hyprctl clients | grep -A 1 "initialClass: steam"  | grep 'initialTitle:' | grep -c "Steam")
+
+if [[ $steamMenu -ge 2 ]] && [[ $IsSteamMenu == 1 ]]; then
+  let clients-=1
+fi
+
+if [[ steamNotification > 0 ]]; then
+  let clients-=steamNotification
+fi
 
 if [[ "$clients" -ge "2" ]]; then
   state="warning"
