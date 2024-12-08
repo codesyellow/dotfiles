0f3dbf9398740f41311e78c77db2c6c5fe184fe9
digo <aedigo12@gmail.com>
2024-12-08 11:52:06 -0300

    some changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.nvim_tree_respect_buf_cwd = 1
-vim.o.wrap = true
-vim.cmd("set background=dark")
-vim.g.maplocalleader = ";"
-vim.bo.filetype = "sh"
+vim.g.lazyvim_python_lsp = "pyright"
