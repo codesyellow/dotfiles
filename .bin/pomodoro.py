441ae73249b986244ae01b98eb87b5ef99a7fc5a
digo <aedigo12@gmail.com>
2025-01-06 07:25:40 -0300

    refacture the code

--- .bin/pomodoro.py
+++ .bin/pomodoro.py
@@ -1,92 +1,82 @@
 #!/usr/bin/env python
-import time
+from time import sleep
 import os
 from datetime import timedelta
 
-
-if os.path.isfile("/tmp/pomo_timer"):
-    print("Pomodoro is already running!")
-    exit(0)
-POMODORO_TIME = 25
-POMODORO_PAUSE = 5
-POMODORO_LONG_PAUSE = 20
-BREAKS_BEFORE_LONG_PAUSE = 4
-POMODORO_TIME_PATH = "/tmp/pomo_timer"
-POMODORO_LONG_PAUSE_PATH = "/tmp/pomo_long_pause"
-POMODORO_PAUSE_PATH = "/tmp/pomo_pause"
 HOME = os.path.expanduser("~/")
 
-pomodoro_running = True
-pomodoro_countdown = POMODORO_TIME * 60
-pomodoro_pause_countdown = POMODORO_PAUSE * 60
-pomodoro_long_pause_countdown = POMODORO_LONG_PAUSE * 60
-number_of_breaks = 0
-should_take_break = False
-should_take_long_break = False
 
+class Pomodoro():
+    def __init__(self) -> None:
+        self.number_of_breaks = 0
+        self.pomodoro = 25 * 60
+        self.pomodoro_pause = 5 * 60
+        self.pomodoro_long_pause = 20 * 60
+        self.breaks_before_long_pause = 4
+        self.pomodoro_time_path = "/tmp/pomo_timer"
+        self.pomodoro_pause_path = "/tmp/pomo_pause"
+        self.created_pomo_pause = False
+        self.cleanup()
 
-def play_audio(audio_name):
-    os.system(f'paplay --volume=65536 {HOME}/.audios/{audio_name}')
+    def run(self):
+        for times in range(self.breaks_before_long_pause):
+            if times <= 2:
+                self.play_audio("pomo_start.wav")
+                self.countdown(time=self.pomodoro, name="pomo")
+                self.play_audio("pomo_pause.wav")
+                self.countdown(time=self.pomodoro_pause, name="pausa")
+                self.number_of_breaks += 1
+            else:
+                self.play_audio("pomo_start.wav")
+                self.countdown(time=self.pomodoro, name="pomo")
+                self.play_audio("pomo_pause.wav")
+                self.number_of_breaks += 1
+                self.countdown(time=self.pomodoro_long_pause, name="pausa")
+                self.play_audio("pomo_end.wav")
+                self.cleanup()
 
+    def cleanup(self):
+        if self.file_exist(self.pomodoro_time_path):
+            self.remove_file(self.pomodoro_time_path)
+        elif self.file_exist(self.pomodoro_pause_path):
+            self.remove_file(self.pomodoro_pause_path)
 
-play_audio("pomo_start.wav")
+    def countdown(self, time, name):
+        while time > 0:
+            if name == "pausa" and not self.created_pomo_pause:
+                self.created_pomo_pause = True
+                f = open(self.pomodoro_pause_path, mode="w")
+                f.close()
+            elif name == "pomo" and self.created_pomo_pause:
+                self.created_pomo_pause = False
+                self.remove_file(self.pomodoro_pause_path)
+            self.save_time(time)
+            time -= 1
+            sleep(1)
 
-
-def reset_time(time):
-    return time * 60
-
-
-def save_time(time):
-    with open(POMODORO_TIME_PATH, mode="w") as pomo_file:
-        minutes = str(timedelta(seconds=time))[2:]
-
-        pomo_file.write(
-            f"{number_of_breaks}[{minutes}]")
-
-
-def remove_file(path):
-    if os.path.exists(path):
-        os.remove(path)
-
-
-while pomodoro_running:
-    if os.path.exists("/tmp/pomo_cancel"):
-        break
-
-    if should_take_break:
-        pomodoro_pause_countdown -= 1
-        save_time(pomodoro_pause_countdown)
-        if number_of_breaks >= BREAKS_BEFORE_LONG_PAUSE:
-            should_take_long_break = True
-            should_take_break = False
-        elif pomodoro_pause_countdown <= 0:
-            play_audio("pomo_pause.wav")
-            if os.path.exists(POMODORO_PAUSE_PATH):
-                os.remove(POMODORO_PAUSE_PATH)
-            should_take_break = False
-            number_of_breaks += 1
-            pomodoro_pause_countdown = reset_time(POMODORO_PAUSE)
+    def is_running(self):
+        if os.path.isfile(self.pomodoro_time_path):
+            return True
         else:
-            open(POMODORO_PAUSE_PATH, "w")
-    elif should_take_long_break:
-        pomodoro_long_pause_countdown -= 1
-        save_time(pomodoro_long_pause_countdown)
-        open(POMODORO_LONG_PAUSE_PATH, "w")
-        if pomodoro_long_pause_countdown <= 0:
-            play_audio("pomo_pause.wav")
-            pomodoro_running = False
-            break
-    else:
-        pomodoro_countdown -= 1
-        save_time(pomodoro_countdown)
+            return False
 
-        if pomodoro_countdown <= 0:
-            play_audio("pomo_pause.wav")
-            should_take_break = True
-            pomodoro_countdown = reset_time(POMODORO_TIME)
+    def play_audio(self, audio_name):
+        os.system(f'paplay --volume=65536 {HOME}/.audios/{audio_name}')
 
-    time.sleep(1)
+    def save_time(self, time):
+        with open(self.pomodoro_time_path, mode="w") as pomo_file:
+            minutes = str(timedelta(seconds=time))[2:]
 
-remove_file(POMODORO_LONG_PAUSE_PATH)
-remove_file(POMODORO_TIME_PATH)
-remove_file("/tmp/pomo_cancel")
+            pomo_file.write(
+                f"{self.number_of_breaks}[{minutes}]")
+
+    def remove_file(self, path):
+        if os.path.exists(path):
+            os.remove(path)
+
+    def file_exist(self, file_name):
+        os.path.exists(f"/tmp/{file_name}")
+
+
+if __name__ == "__main__":
+    Pomodoro().run()
