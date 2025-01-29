04cbb19463949771a5f06af648e08c258a9fa6d2
digo <aedigo12@gmail.com>
2025-01-29 20:29:30 -0300

    changed the position

--- .bin/restore_layout.sh
+++ .bin/restore_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --mode 1360x768 --pos 0x0 --rotate left --output HDMI-1 --primary --mode 1280x1024 --pos 768x536 --rotate normal --output DP-1 --off
+xrandr --output VGA-1 --mode 1360x768 --pos 0x0 --rotate left --output HDMI-1 --primary --mode 1280x1024 --pos 768x504 --rotate normal --output DP-1 --off
