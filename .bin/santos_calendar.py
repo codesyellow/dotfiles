e7ed58c92c789240fe1485d93bc978398ca03b65
digo <aedigo12@gmail.com>
2025-02-12 15:32:59 -0300

    added a notification for the pre_game

--- .bin/santos_calendar.py
+++ .bin/santos_calendar.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/home/digo/.bin/.venv/bin/python
 from requests_html import HTMLSession
 import os
 import json
 from typing import Dict
+import subprocess
 from datetime import datetime
+from time import sleep
 
 HOME = os.path.expanduser("~")
 SCHEDULE_PATH = f"{HOME}/.config/sSchedule.json"
@@ -12,7 +14,9 @@ SCHEDULE_PATH = f"{HOME}/.config/sSchedule.json"
 class Santos():
     def __init__(self) -> None:
         self.schedule: list = self.get_schedule()
-        self.game_info: str = ""
+        self.pre_game_hour: str = self.get_pregame_hour()
+        self.game_hour: str = str(self.get_next_game()["hour"])
+        self.pre_game_state = False
 
     def scrap_schedule(self) -> list:
         """Scrap the data return it"""
@@ -64,8 +68,12 @@ class Santos():
     def update_schedule(self) -> None:
         """Update the schedule"""
         new_schedule = self.scrap_schedule()
-        self.make_copy(self.schedule)
-        self.save_schedule(new_schedule)
+        if len(new_schedule) > 0:
+            self.make_copy(self.schedule)
+            self.save_schedule(new_schedule)
+        else:
+            print(
+                "There was a problem with the new scrap data, since it's empty and won't be saved.")
 
     def get_next_game(self) -> Dict[str, int]:
         """Get the next game and return it"""
@@ -77,10 +85,40 @@ class Santos():
         current_day = datetime.now().day
         game_day = self.get_next_game()["day"]
 
+        print(current_day, game_day)
         if current_day == game_day:
             return True
         else:
             return False
+
+    def get_pregame_hour(self) -> str:
+        """Return the pregame hour as a string"""
+        game = self.get_next_game()
+        game_hour = int(str(game["hour"]).split(":")[0]) - 2
+        game_minutes = str(game["hour"]).split(":")[1]
+        return f"{game_hour}:{game_minutes}"
+
+    def pre_game(self) -> bool:
+        """Return True if it's the time of the pregame"""
+        hour = datetime.now().hour
+        minutes = datetime.now().minute
+        current_hour = f"{hour}:{minutes}"
+
+        start = datetime.strptime(current_hour, "%H:%M")
+        end = datetime.strptime(self.pre_game_hour, "%H:%M")
+
+        difference = end - start
+        seconds = difference.total_seconds()
+        return seconds // 60 <= 120
+
+    def send_notification(self, message):
+        """Send a notification"""
+        subprocess.run([
+            "notify-send",
+            "-u",
+            "critical",
+            message
+        ])
 
     def write(self):
         """Write to the file the next game information"""
@@ -92,9 +130,11 @@ class Santos():
             away_team = next_game["away_team"]
             if self.is_game_today():
                 if home_team == "Santos":
-                    self.game_info = f"{game_day}:{game_hour}/{away_team}(H)"
+                    self.game_info = f"{game_day}:{
+                        game_hour}/{away_team[:3]}(H)"
                 elif away_team == "Santos":
-                    self.game_info = f"{game_day}:{game_hour}/{home_team}(A)"
+                    self.game_info = f"{game_day}:{
+                        game_hour}/{home_team[:3]}(A)"
             else:
                 if home_team == "Santos":
                     self.game_info = f"{game_day}:{game_hour}(H)"
@@ -104,7 +144,14 @@ class Santos():
             data.write(self.game_info)
 
     def run(self) -> None:
+        self.update_schedule()
         self.write()
+        if self.is_game_today():
+            while True:
+                if self.pre_game() and not self.pre_game_state:
+                    self.send_notification("Pre game!")
+                    self.pre_game_state = True
+                sleep(60)
 
 
 if __name__ == "__main__":
