79d487e90d3d6a35c01de497dc0591e19f413bc5
digo <aedigo12@gmail.com>
2025-02-20 18:47:21 -0300

    added a countdown widget

--- .config/waybar/custom_widgets/pomo.sh
+++ .config/waybar/custom_widgets/pomo.sh
@@ -2,13 +2,13 @@
 BAR_COLOR="#4c566a"
 NORMAL_COLOR="#d8dee9"
 WARNING_COLOR="#EF5A6F"
-icon_symbol=""
+icon_symbol=""
 bary=$((8 * 1000))
 icony=$((8 * 1000))
 texty=$((7 * 1000))
 bsize=$((15 * 1000))
 tsize=$((12 * 1000))
-isize=$((11 * 1000))
+isize=$((13 * 1000))
 bar="<span size='$bsize' rise='$bary' foreground='$BAR_COLOR'> |</span>"
 icon="<span size='$isize' rise='$icony' foreground='$NORMAL_COLOR'>$icon_symbol</span>"
 
@@ -22,11 +22,11 @@ if [[ -f "/tmp/pomo_timer" ]]; then
         icon="<span size='$isize' rise='$icony' foreground='$NORMAL_COLOR'>$icon_symbol</span>"
         text="<span size='$tsize' rise='$texty' foreground='$NORMAL_COLOR'>$pomo</span>"
     fi
-    output="{\"text\": \"$bar $icon $text\", \"tooltip\": \"Updates: $ds4_bat\"}"
+    output="{\"text\": \"$bar $icon $text\", \"tooltip\": \"Updates: $pomo\"}"
 
     echo $output
 else
     text="<span size='$tsize' rise='$texty' foreground='$NORMAL_COLOR'>0:00:00</span>"
-    output="{\"text\": \"$bar $icon $text\", \"tooltip\": \"Updates: $ds4_bat\"}"
+    output="{\"text\": \"$bar $icon $text\", \"tooltip\": \"Updates: $pomo\"}"
     echo $output
 fi
