778bffbaaa4c864e60d17ed6e49451459ea1dd96
digo <aedigo12@gmail.com>
2025-03-01 21:26:05 -0300

    changed the if statements to case

--- .bin/volume.sh
+++ .bin/volume.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
-if [ "$1" == "up" ]; then
-  pactl set-sink-volume @DEFAULT_SINK@ +5%
-fi
 
-if [ "$1" == "down" ]; then
-  pactl set-sink-volume @DEFAULT_SINK@ -5%
-fi
-
-if [ "$1" == "mute" ]; then
-  pactl set-sink-mute @DEFAULT_SINK@ toggle
-fi
+case "$1" in
+"up")
+    pactl set-sink-volume @DEFAULT_SINK@ +5%
+    ;;
+"up_slow")
+    pactl set-sink-volume @DEFAULT_SINK@ +1%
+    ;;
+"down")
+    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    ;;
+"down_slow")
+    pactl set-sink-volume @DEFAULT_SINK@ -1%
+    ;;
+"mute")
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    ;;
+esac
 
 VOLUME=$(pamixer --get-volume)
 MUTE=$(echo $AMIXER | grep -o '\[off\]' | tail -n 1)
 if [ "$VOLUME" -le 20 ]; then
-  ICON=audio-volume-low
+    ICON=audio-volume-low
 else
-  if [ "$VOLUME" -le 60 ]; then
-    ICON=audio-volume-medium
-  else
-    ICON=audio-volume-high
-  fi
+    if [ "$VOLUME" -le 60 ]; then
+        ICON=audio-volume-medium
+    else
+        ICON=audio-volume-high
+    fi
 fi
 if [ "$MUTE" == "[off]" ]; then
-  ICON=audio-volume-muted
+    ICON=audio-volume-muted
 fi
 
 notify-send.sh $VOLUME% \
-  --replace=22 \
-  -u low \
-  -a volume \
-  -i /usr/share/icons/AdwaitaLegacy/32x32/legacy/$ICON.png
+    --replace=22 \
+    -u low \
+    -a volume \
+    -i /usr/share/icons/AdwaitaLegacy/32x32/legacy/$ICON.png
