2669481ab405ef1e131497007e583a7c0231126e
digo <aedigo12@gmail.com>
2025-03-15 20:18:08 -0300

    some changes

--- .config/waybar/custom_widgets/updates.py
+++ .config/waybar/custom_widgets/updates.py
@@ -3,18 +3,20 @@ import json
 import subprocess
 from config import get_pango
 
-updates_available = subprocess.check_output(
-    "checkupdates"
-).decode("utf-8").count("\n")
+try:
+    updates_available = subprocess.check_output(
+        "checkupdates"
+    ).decode("utf-8").count("\n")
+except subprocess.CalledProcessError:
+    pass
+else:
+    classes = ["normal", "updates"]
+    if updates_available > 20:
+        classes[0] = "warning"
+    output = f"{get_pango('UP', updates_available)}"
 
-
-classes = ["normal", "updates"]
-if updates_available > 20:
-    classes[0] = "warning"
-output = f"{get_pango('UP', updates_available)}"
-
-print(json.dumps({
-    "text": output,
-    "tooltip": f"Updates: {updates_available}",
-    "class": classes
-}))
+    print(json.dumps({
+        "text": output,
+        "tooltip": f"Updates: {updates_available}",
+        "class": classes
+    }))
