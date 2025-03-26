6d2eb25f0c0a6f4cbcc6e2ca7d298a730324d121
digo <aedigo12@gmail.com>
2025-03-26 12:55:17 -0300

    changed from xdotool to ydotool

--- .bin/360.sh
+++ .bin/360.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 sleep 3
-# Define a variable to store the value
+
 value=0
 
-# Function to display usage information
 usage() {
-  echo "Usage: $0 <increment>"
-  echo "Example: $0 10"
+    echo "Usage: $0 <increment>"
+    echo "Example: $0 10"
 }
 
-# Check if an argument is provided
 if [ $# -ne 1 ]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
-# Loop for 10 seconds
 for ((i = 0; i < 10; i++)); do
-  # Increment the value by the argument passed
-  value=$((value + $1))
-  xdotool mousemove_relative -- $value 0
-  echo "Current value: $value"
-  sleep 1 # Adjust the sleep duration as needed
+    value=$((value + $1))
+    ydotool mousemove $value 0
+    echo "Current value: $value"
+    sleep 1
 done
-
-echo "Script finished."
