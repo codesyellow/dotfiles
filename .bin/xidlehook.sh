f9eb69e5ea338d6513dbb731b71e5466e7e46138
digo <aedigo12@gmail.com>
2025-05-11 07:13:53 -0300

    changes

--- .bin/xidlehook.sh
+++ .bin/xidlehook.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-# Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-
-# Run xidlehook
 xidlehook \
   `# Don't lock when there's a fullscreen application` \
   --not-when-fullscreen \
   --not-when-audio \
+  --timer 150 \
+  'betterlockscreen -l' \
+  '' \
   --timer 300 \
-  'betterlockscreen -l; xmon' \
-  ''
+  'xset -display :0.0 dpms force off' \
+  'xset -display :0.0 dpms force on'
