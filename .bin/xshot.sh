d0fb1c3eed0fd61b801f4b81bd1d937362f7ae17
digo <aedigo12@gmail.com>
2025-12-15 13:10:02 -0300

    switched to flameshot instead of scrot

--- .bin/xshot.sh
+++ .bin/xshot.sh
@@ -1,34 +1,18 @@
 #!/usr/bin/env bash
 
 PIC_PATH="$HOME/.pictures"
-
 mkdir -p "$PIC_PATH"
+
 DATE="$(date +'%Y%m%d%H%M%S')"
+OUT="$PIC_PATH/ps_$DATE.png"
 
-if [[ "$1" == "region" ]]; then
-  scrot -s "$PIC_PATH/ps_$DATE.jpg" \
-    && dunstify "Region screenshot was taken!"
+case "$1" in
+  screen)
+    sleep 1
+    flameshot screen -p "$OUT" 
+    ;;
 
-elif [[ "$1" == "zoom" ]]; then
-  TMP="$(mktemp).png"
-  scrot -s "$TMP"
-  convert "$TMP" -resize 200% -filter Mitchell "$TMP"
-  imv -u nearest_neighbour "$TMP"
-  rm "$TMP"
-
-elif [[ "$1" == "mon" ]]; then
-  sleep 1
-  scrot -m "$PIC_PATH/ps_mon_$DATE.jpg" \
-    && dunstify "Screenshot of current monitor was taken"
-
-elif [[ "$1" == "edit" ]]; then
-  TMP="$(mktemp).png"
-  sleep 1
-  scrot -s "$TMP"
-  gimp "$TMP" &
-
-else
-  sleep 1
-  scrot "$PIC_PATH/ps_$DATE.png" \
-    && dunstify "Full screenshot was taken!"
-fi
+  region|edit)
+    flameshot gui -p "$OUT"
+    ;;
+esac
