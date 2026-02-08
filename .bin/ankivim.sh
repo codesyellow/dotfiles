9caaf32a73fc2ba91ffff821929b42101aeef64f
digo <aedigo12@gmail.com>
2026-02-08 18:37:06 -0300

    changed from wofi to wmenu

--- .bin/ankivim.sh
+++ .bin/ankivim.sh
@@ -4,10 +4,11 @@ search_dir="$HOME/.ankivim/decks"
 
 mkdir -p "$search_dir"
 
-folder_name=$(find "$search_dir" -maxdepth 1 -type d | sed "s|$search_dir/||" | tail -n +2 | wofi --dmenu --prompt "")
+folder_name=$(find "$search_dir" -maxdepth 1 -type d \
+  | sed "s|$search_dir/||" \
+  | tail -n +2 \
+  | wmenu -f "Victor Mono Bold Italic 14" -N "#272E33" -n "#D3C6AA" -S "#272E33" -s "#E69875" -i -l "5" -p "AnkiVim")
 
 [ -z "$folder_name" ] && exit
-
-mkdir -p "$folder_path"
 
 foot -T 'ankivim' -a "ankivim" -e anki-vim "$folder_name"
